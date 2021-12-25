97291d52da4036815c3d935912876d69e1d52b50
unknown <eluewisdom@gmail.com>
2021-12-25 07:32:53 -0800

    changes

--- src/react-hook-form/Form.js
+++ src/react-hook-form/Form.js
@@ -8,7 +8,19 @@ function Form() {
         console.log(data)
         document.getElementById('data').textContent = `EMAIL: ${data.email}, PASSWORD: ${data.password}`
     }
-    
+
+    const handleEye = () => {
+        const password = document.querySelector('.password')
+        const eye = document.getElementById('eye')
+        if(password.type === 'password'){
+            password.setAttribute('type','text')
+            eye.className = 'fas fa-eye'
+        } else {
+            password.setAttribute('type', 'password')
+            eye.className = 'fas fa-eye-slash'
+        }
+    }
+
     return (
         <form style={{margin: '2rem'}} onSubmit={handleSubmit(onSubmit)}>
             <input type='email'
@@ -16,15 +28,18 @@ function Form() {
               name='email'
               {...register('email', {required: true})}
             />
-            {errors.email && <p style={styles.error}>Enter a valid email</p>}
+            {errors.email && <p styles={styles.error}>Enter a valid email</p>}
             <br/>
+            <div style={styles.flex}>
             <input type='password'
               placeholder='Password'
               name='password'
+              className='password'
               {...register('password', {required: 'Password cannot be blank',
                minLength: {value: 8, message: 'Password must be at least 8 characters'},
                maxLength: {value: 10, message: 'Password must not be more than 10 characters'}})}
             />
+            <p style={styles.eye} onClick={handleEye}><i id='eye' className="fas fa-eye-slash"></i></p></div>
             {errors.password && <p style={styles.error}>{errors.password.message}</p>}
             <br/>
             <input type='submit' className='submit' />
@@ -37,7 +52,18 @@ const styles = {
     error: {
         color: 'red',
         fontWeight: '700'
+    },
+
+    flex: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+
+    eye: {
+        marginLeft: '.5rem',
+        cursor: 'pointer'
     }
+
 }
 
 export default Form
